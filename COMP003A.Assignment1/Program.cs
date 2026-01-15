ade9c5bc57b2ac7b972c23db142a8278d0125221
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-01-15 15:31:16 -0800

    Added items 1-10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("I Like Reading");
+            Console.WriteLine("I Am Filipino");
+            Console.WriteLine("I Love Sinigang");
+            Console.WriteLine("I Love Working Out");
+            Console.WriteLine("I Love Playing Video Games");
+            Console.WriteLine("I Love Cars");
+            Console.WriteLine("I Like Writing");
+            Console.WriteLine("I Love Math");
+            Console.WriteLine("I Love Coding");
+            Console.WriteLine("I Like Doing Projects");
         }
     }
 }
