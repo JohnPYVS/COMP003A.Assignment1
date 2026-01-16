0bf2f58f87118c7dc947f5dba80d5545e7aec8ed
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-01-15 16:14:58 -0800

    Added 11-20 items

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -15,6 +15,17 @@
             Console.WriteLine("I Love Math");
             Console.WriteLine("I Love Coding");
             Console.WriteLine("I Like Doing Projects");
+            Console.WriteLine("I Love Watching Basketball");
+            Console.WriteLine("I Love Watching Football");
+            Console.WriteLine("I Love Watching Boxing");
+            Console.WriteLine("I Am A Curious Guy");
+            Console.WriteLine("I Am A Middle Child");
+            Console.WriteLine("I Love Listening To Music");
+            Console.WriteLine("My Favorite Game is God Of War");
+            Console.WriteLine("I Am A Fan Of Kendrick Lamar");
+            Console.WriteLine("I Am A Fan Of Bruno Mars");
+            Console.WriteLine("I Am A Fan Of Daniel Caesar");
+            Console.WriteLine("My Favorite Music Genre is R&B");
         }
     }
 }
